fce22a28827e5cd5847770cfce47440ab56111c5
W59004 <bwilk163@gmail.com>
2020-03-07 10:19:09 +0100

    initt

---
+++ UITMUniversity/Models/UniversityContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace UITMUniversity.Models
+{
+
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
